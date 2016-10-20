f1e7e547b269080b2f73b6010747ee9a28bcfe14
Akira Takeguchi <a.takeguchi.sf@gmail.com>
2016-10-21 02:24:39 +0900

    sendltv

--- src/ios/FOXiOS.m
+++ src/ios/FOXiOS.m
@@ -14,10 +14,39 @@
 
 - (void)sendLTV:(CDVInvokedUrlCommand *)command
 {
+    NSString* conversionID = [command.arguments objectAtIndex:0];
+    
+    if (conversionID != nil) {
+        [self.commandDelegate runInBackground:^{
+            AppAdForceLtv *ltv = [[AppAdForceLtv alloc] init];
+            [ltv sendLtv:[conversionID intValue]];
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];;
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }];
+    } else {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"conversionID was null"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)sendLTVPurchase:(CDVInvokedUrlCommand *)command
 {
+    NSString* conversionID = [command.arguments objectAtIndex:0];
+    NSString* price = [command.arguments objectAtIndex:1];
+    NSString* currency = [command.arguments objectAtIndex:2];
+    
+    if (conversionID != nil && price != nil && currency != nil) {
+        [self.commandDelegate runInBackground:^{
+            AppAdForceLtv *ltv = [[AppAdForceLtv alloc] init];
+            [ltv addParameter:LTV_PARAM_PRICE:price];
+            [ltv addParameter:LTV_PARAM_CURRENCY:currency];
+            [ltv sendLtv:[conversionID intValue]];
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];;
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }];
+    } else {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"args were null"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }
 }
 
 
