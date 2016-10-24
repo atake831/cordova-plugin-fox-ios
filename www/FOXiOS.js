eb1dc29f0bff66056450ce80bfb67d68d873cf8e
Akira Takeguchi <a.takeguchi.sf@gmail.com>
2016-10-24 18:23:21 +0900

    LTVPurchaseのfunc間違え

--- www/FOXiOS.js
+++ www/FOXiOS.js
@@ -5,5 +5,5 @@ exports.sendLTV = function(conversionID, success, error) {
 };
 
 exports.sendLTVPurchase = function(conversionID, price, currency, itemName, success, error) {
-    exec(success, error, "FOXiOS", "sendLTV", [conversionID, price, currency, itemName]);
+    exec(success, error, "FOXiOS", "sendLTVPurchase", [conversionID, price, currency, itemName]);
 };
